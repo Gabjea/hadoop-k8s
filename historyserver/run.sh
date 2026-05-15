dcb0e85e0e86db21cba3db30cc8f20d6ecab22d1
Gabjea <gabjea856@gmail.com>
2026-05-15 22:29:38 +0300

    bypass leveldb

--- historyserver/run.sh
+++ historyserver/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Force the State Store to use FileSystem instead of LevelDB
+export YARN_CONF_yarn_timeline___service_state___store___class=org.apache.hadoop.yarn.server.timeline.recovery.FileSystemTimelineStateStore
+
+# Double check your store class is properly overriding the default
+export YARN_CONF_yarn_timeline___service_store___class=org.apache.hadoop.yarn.server.timeline.FileSystemTimelineStore
+
 $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR historyserver
